0020dd5eb3a26f562e6190078bfee7522e516c75
pacholoamit <pacholoamit.tech@gmail.com>
2022-06-25 04:09:47 +0800

    create database config

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,1 +1,23 @@
 package config
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect()  {
+	d, err := gorm.Open(sqlite.Open("GO-TASK-MGR.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect to database")
+	}
+	
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
