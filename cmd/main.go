adcd3c55078bd5e5c3a149dfe1b92162e44cc5c9
pacholoamit <pacholoamit.tech@gmail.com>
2022-06-26 06:16:32 +0800

    remove websocket routes

--- cmd/main.go
+++ cmd/main.go
@@ -15,6 +15,5 @@ func main() {
 	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(20))) // 20 request/sec rate limit
 
 	routes.TaskRoutes(e)
-	routes.TaskWebSockets(e)
 	e.Logger.Fatal(e.Start(":8081"))
 }
