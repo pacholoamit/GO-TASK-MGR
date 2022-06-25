c43abfb9c1c782b6e5a06c0c8f93b80e2557a80d
pacholoamit <pacholoamit.tech@gmail.com>
2022-06-26 06:16:17 +0800

    remove websockets

--- pkg/routes/tasks.routes.go
+++ pkg/routes/tasks.routes.go
@@ -4,7 +4,6 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/pacholoamit/GO-TASK-MGR/pkg/controllers"
 	"github.com/pacholoamit/GO-TASK-MGR/pkg/middlewares"
-	"github.com/pacholoamit/GO-TASK-MGR/pkg/websockets"
 )
 
 var TaskRoutes = func(router *echo.Echo) {
@@ -13,8 +12,4 @@ var TaskRoutes = func(router *echo.Echo) {
 	router.GET("/task/:id", controllers.GetTask, middlewares.ValidateQueryId)
 	// router.PUT("/task/:id", controllers.UpdateTask)
 	router.DELETE("/task/:id", controllers.DeleteTask, middlewares.ValidateQueryId)
-}
-
-var TaskWebSockets = func(router *echo.Echo) {
-	router.GET("/websocket", websockets.GetAllTasks)
 }
