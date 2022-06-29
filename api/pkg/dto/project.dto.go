88821139a1cc69a05349f149a5d34d6177eac38e
pacholoamit <pacholoamit.tech@gmail.com>
2022-06-30 07:54:50 +0800

    Add project dto

--- api/pkg/dto/project.dto.go
+++ api/pkg/dto/project.dto.go
@@ -1,6 +1,12 @@
 package dto
 
+import "time"
+
 type Project struct {
-	Name  string `json:"name" form:"name"`
-	Color string `json:"color" form:"color"`
+	ID        uint `gorm:"primarykey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt time.Time
+	Name      string `json:"name" form:"name"`
+	Color     string `json:"color" form:"color"`
 }
--- api/pkg/dto/task.dto.go
+++
@@ -1,9 +0,0 @@
-package dto
-
-type Task struct {
-	Title       string `json:"title" form:"title" validate:"required"`
-	Description string `json:"description" form:"description" validate:"required"`
-	Status      string `json:"status" form:"status"`
-	Label       string `json:"label" form:"label"`
-	ProjectID   int
-}
