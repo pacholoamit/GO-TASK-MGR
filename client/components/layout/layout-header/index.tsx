a1a15db73b7cdb629de8a8814a6dea309a672c0b
pacholoamit <pacholoamit.tech@gmail.com>
2022-07-01 08:29:56 +0800

    Colorscheme toggle now working

--- client/components/layout/layout-header/index.tsx
+++ client/components/layout/layout-header/index.tsx
@@ -1,6 +1,14 @@
-import { Group, Header, Text, Title } from "@mantine/core";
+import {
+  ActionIcon,
+  Group,
+  Header,
+  Text,
+  Title,
+  useMantineColorScheme,
+} from "@mantine/core";
 import Image from "next/image";
 import { CSSProperties } from "react";
+import { MoonStars, Sun } from "tabler-icons-react";
 
 type styles = {
   [key: string]: CSSProperties;
@@ -16,12 +24,30 @@ const styles: styles = {
   },
 };
 
+const ColorSchemeToggle = () => {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const dark = colorScheme === "dark";
+
+  return (
+    <ActionIcon
+      variant="outline"
+      onClick={() => toggleColorScheme()}
+      title="Toggle color scheme"
+    >
+      {dark ? <Sun size={18} /> : <MoonStars size={18} />}
+    </ActionIcon>
+  );
+};
+
 const LayoutHeader: React.FC = () => {
   return (
     <Header height={72} p="xs">
-      <Group style={styles.group} spacing="xs" align={"center"}>
-        <Image src="/gopher.svg" alt="go logo" width="36" height="48" />
-        <Title style={styles.title}>GO TASK MGR</Title>
+      <Group position="apart">
+        <Group style={styles.group} spacing="xs" align={"center"}>
+          <Image src="/gopher.svg" alt="go logo" width="36" height="48" />
+          <Title style={styles.title}>GO TASK MGR</Title>
+        </Group>
+        <ColorSchemeToggle />
       </Group>
     </Header>
   );
--- client/providers/theme.provider.tsx
+++ client/providers/theme.provider.tsx
@@ -1,21 +1,37 @@
 import React from "react";
-import { MantineProvider, MantineThemeOverride } from "@mantine/core";
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+  MantineThemeOverride,
+} from "@mantine/core";
 
 type ThemeProviderProps = {
   children: React.ReactNode;
 };
 const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
+  const [colorScheme, setColorScheme] = React.useState<ColorScheme>("light");
+
   const theme: MantineThemeOverride = {
-    colorScheme: "dark",
     headings: {
       fontFamily: "'League Spartan', sans-serif;",
     },
+    colorScheme,
   };
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
   return (
     <>
-      <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
-        {children}
-      </MantineProvider>
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
+          {children}
+        </MantineProvider>
+      </ColorSchemeProvider>
     </>
   );
 };
